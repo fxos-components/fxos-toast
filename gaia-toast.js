89776881f615d2db85bf5df7a581c9906b8dfc61
Timothy Guan-tin Chien <timdream@gmail.com>
2015-04-13 17:59:40 +0800

    Remove the old event listener

--- gaia-toast.js
+++ gaia-toast.js
@@ -44,6 +44,7 @@ module.exports = component.register('gaia-toast', {
     this.els.bread.classList.remove('animate-in');
     this.els.bread.classList.add('animate-out');
 
+    this.els.bread.removeEventListener('animationend', this.onAnimateOutEnd);
     this.onAnimateOutEnd = function() {
       self.els.bread.removeEventListener('animationend', self.onAnimateOutEnd);
       self.els.bread.classList.remove('animate-out');
